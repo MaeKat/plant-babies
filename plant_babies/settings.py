a8f810a384322f2d50cbe25c1d80ceae20b525f2
carolakey <carol.key42@gmail.com>
2020-04-26 21:52:47 -0500

    fixing settings for static files

--- plant_babies/settings.py
+++ plant_babies/settings.py
@@ -195,19 +195,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-location = lambda x: os.path.join(
-os.path.dirname(os.path.realpath(__file__)), x)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-location = lambda x: os.path.join(
-os.path.dirname(os.path.realpath(__file__)), x)
 STATIC_URL = '/static/'
-STATIC_ROOT = location('static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = location('media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 THUMBNAIL_DEBUG = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'statics'),
 ]
-
-
